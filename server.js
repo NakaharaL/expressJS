1e16f751b95aaea436397d93dcfae87154061c0a
Leonardo Nakahara <leonardo.nakahara@gmail.com>
2024-08-17 10:20:04 -0300

    Update

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import posts from './routes/posts.js';
+import logger from './middleware/logger.js';
+import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFound.js';
+const port = process.env.PORT || 8000;
+
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Logger middleware
+app.use(logger);
+
 // setup static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-let posts = [
-    {id: 1, title: 'Post One'},
-    {id: 2, title: 'Post Two'},
-    {id: 3, title: 'Post Three'},
-];
+// Routes
+app.use('/api/posts', posts);
 
-app.get('/api/posts', (req, res) => {
-    res.json(posts);
-});
+// Error handler
+app.use(notFound);
+app.use(errorHandler);
 
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
-})
+app.listen(port, () => console.log(`Server is running on port ${port}`));
